4f0e63b64506f1e9e160921126eaaec56686d6c9
Vartexy <49254739+Vartexy@users.noreply.github.com>
2021-11-20 19:57:11 +0000

    Minor tweaks to director requests - currently not working now but aiming to fix tonight

--- movietest.py
+++ movietest.py
@@ -16,6 +16,8 @@ class Game:
 		self._filmIds = []
 		self._cScore = 0
 		self._topScores = {}
+		self._directorMap = self.get_director_and_film()
+		self._filmTitles = self._directorMap.items()
 		
 		#file = open("highscores.txt","a+")
 		#scoreDump = file.readlines()
@@ -27,29 +29,44 @@ class Game:
 		# Take the top 20 films from the movieData list and append them to their respective attributes
 		for i in range(20): 
 			self._filmIds.append(self._movieData['results'][i]['id'])
-			self._filmTitles.append((self._movieData['results'][i]['title']))
 
 		print("\nSorted films...")
 
 	def get_director_and_film(self):
 		
 		print("\nGetting Director")
-		info = []
+		directors = []
+		titles = []
+		infoMap = {}
 
 		# Get a random director from the list of film IDs
-		j = randint(0,len(self._filmIds) - 1)
-		credits = "https://api.themoviedb.org/3/movie/" + str(self._filmIds[j]) + "/credits?api_key=" + self._apiKey 
-		movie = "https://api.themoviedb.org/3/movie/" + str(self._filmIds[j]) + "?api_key=" + self._apiKey
+		for x in self._filmIds:
+			credits = "https://api.themoviedb.org/3/movie/" + str(x) + "/credits?api_key=" + self._apiKey 
+			movie = "https://api.themoviedb.org/3/movie/" + str(x) + "?api_key=" + self._apiKey
 
-		# Loop through the credits until the director is found
-		for i in range(30):
+			# Loop through the credits until the director is found
+			try:
+				for i in range(30):
 
-			if requests.get(credits).json()['crew'][i]['job'] == 'Director':
+					if requests.get(credits).json()['crew'][i]['job'] == 'Director':
 
-				info.append(requests.get(credits).json()['crew'][i]['name'])
+						directors.append(requests.get(credits).json()['crew'][i]['name'])
 
-		info.append(requests.get(movie).json()['original_title'])
-		return info
+			except IndexError:
+
+				break
+
+			titles.append(requests.get(movie).json()['original_title'])
+		
+		i = 0
+		for x in titles:
+
+			infoMap[x] = directors[i]
+			i +=1
+
+		print("\n\nDirectors: " + str(directors))
+		print("\n\nTitles: " + str(titles))
+		return infoMap
 
 	def play(self):
 
@@ -60,18 +77,18 @@ class Game:
 
 		while end == 0:
 
-			info = self.get_director_and_film()
-			print(info)
-			answer = info[0]
-			print("\nQuestion " + str(i) + ":\nWho was the director of the film " + info[1])
+			print(self._filmTitles)
+			print(self._directorMap[self._filmTitles[i]])
+			print("\nCurrent Score: " + str(self._cScore) + "")
+			print("\nQuestion " + str(i+1) + ":\nWho was the director of the film " + self._filmTitles[i])
 			guess = input()
 
-			if answer == guess:
+			if guess == self._directorMap[self._filmTitles[i]]:
 				print("Correct!")
 				i += 1
 				self._cScore += 1
 			else:
-				print("Incorrect! The correct answer was " + answer + "!")
+				print("Incorrect! The correct answer was " + self._directorMap[self._filmTitles[i]] + "!")
 				end = 1
 			
 
